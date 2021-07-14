df3e276a576dc30eb3f5cde6ac08a339ada9f0ea
Tenshiroza007 <85775989+waritthonAuttama@users.noreply.github.com>
2021-07-15 04:21:25 +0700

    add cardView and pull data success

--- app/src/main/java/com/example/newreaderapp/MainActivity.java
+++ app/src/main/java/com/example/newreaderapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.newreaderapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.AsyncTask;
@@ -17,6 +18,7 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,39 +26,138 @@ public class MainActivity extends AppCompatActivity {
     private TextView txtTitle;
     private RecyclerView recyclerView;
 
+    private NewsAdapter adapter;
+    private ArrayList<NewsItem>news;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        recyclerView=findViewById(R.id.recyclerView);
+        news=new ArrayList<>();
+
+        recyclerView = findViewById(R.id.recyclerView);
+        adapter=new NewsAdapter(this);
+
+//        adapter.setNews(news);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        new GetNew().execute();
     }
-    private class GetNew extends AsyncTask<Void,Void,Void>{
+
+    private class GetNew extends AsyncTask<Void, Void, Void> {
 
         @Override
         protected Void doInBackground(Void... voids) {
-            InputStream inputStream=getInputStream();
-            if(null !=inputStream){
+            InputStream inputStream = getInputStream();
+            if (null != inputStream) {
                 try {
                     initXMLPullParser(inputStream);
-                } catch (XmlPullParserException e) {
+                } catch (XmlPullParserException | IOException e) {
                     e.printStackTrace();
                 }
             }
             return null;
         }
 
-        private void initXMLPullParser(InputStream inputStream) throws XmlPullParserException {
-            Log.d(TAG, "initXMLPullParser: Initializing XML Pull Parser");
-            XmlPullParser parser = Xml.newPullParser();
-            parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES,false);
-            parser.setInput(inputStream,null);
+        @Override
+        protected void onPostExecute(Void unused) {
+            super.onPostExecute(unused);
+            Log.d(TAG, "onPostExecute: "+news.toString());
+            adapter.setNews(news);
         }
 
-        private InputStream getInputStream(){
+        private void initXMLPullParser(InputStream inputStream) throws XmlPullParserException, IOException {
+            Log.d(TAG, "initXMLPullParser: Initializing XML Pull Parser");
+            XmlPullParser parser = Xml.newPullParser();
+            parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+            parser.setInput(inputStream, null);
+
+            parser.next();
+            parser.require(XmlPullParser.START_TAG, null, "rss");
+            while (parser.next() != XmlPullParser.END_TAG) {
+                if (parser.getEventType() != XmlPullParser.START_TAG) {
+                    continue;
+                }
+                parser.require(XmlPullParser.START_TAG, null, "channel");
+                while (parser.next() != XmlPullParser.END_TAG) {
+                    if (parser.getEventType() != XmlPullParser.START_TAG) {
+                        continue;
+                    }
+
+                    if (parser.getName().equals("item")) {
+                        String title = "";
+                        String description = "";
+                        String link = "";
+                        String date = "";
+
+                        parser.require(XmlPullParser.START_TAG, null, "item");
+                        while (parser.next() != XmlPullParser.END_TAG) {
+                            if (parser.getEventType() != XmlPullParser.START_TAG) {
+                                continue;
+                            }
+                            String tagName = parser.getName();
+                            if (tagName.equals("title")) {
+                                title=getContext(parser,"title");
+
+                            } else if (tagName.equals("description")) {
+                                description=getContext(parser,"description");
+
+                            } else if (tagName.equals("link")) {
+                                link=getContext(parser,"link");
+
+                            } else if (tagName.equals("pubdate")) {
+                                date=getContext(parser,"pubdate");
+                            }else{
+                                skipTag(parser);
+                            }
+                        }
+                        NewsItem item=new NewsItem(title,description,link,date );
+                        news.add(item);
+
+                    } else {
+                        skipTag(parser);
+                    }
+                }
+            }
+        }
+
+        private String getContext(XmlPullParser parser,String tag) throws IOException, XmlPullParserException {
+            String content="";
+            parser.require(XmlPullParser.START_TAG,null,tag);
+            if(parser.next()==XmlPullParser.TEXT){
+                content=parser.getText();
+                parser.next();
+            }
+            return content;
+        }
+
+        private void skipTag(XmlPullParser parser) throws XmlPullParserException, IOException {
+            if(parser.getEventType() !=XmlPullParser.START_TAG){
+                throw new IllegalStateException();
+            }
+            int number=1;
+            while (number !=0){
+                switch (parser.next()){
+                    case XmlPullParser.START_TAG:
+                        number++;
+                        break;
+                    case XmlPullParser.END_TAG:
+                        number--;
+                        break;
+                    default:
+                        break;
+
+                }
+            }
+        }
+
+        private InputStream getInputStream() {
             try {
-                URL url=new URL("https://www.posttoday.com/rss/src/breakingnews.xml");
-                HttpURLConnection connection= (HttpURLConnection) url.openConnection();
+                URL url = new URL("https://www.posttoday.com/rss/src/breakingnews.xml");
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
                 connection.setRequestMethod("GET");
                 connection.setDoInput(true);
                 return connection.getInputStream();
---
+++ app/src/main/java/com/example/newreaderapp/NewsAdapter.java
@@ -0,0 +1,73 @@
+package com.example.newreaderapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder>{
+    private Context context;
+    private ArrayList<NewsItem>news;
+
+    public NewsAdapter(Context context) {
+        this.context = context;
+    }
+
+    public void setNews(ArrayList<NewsItem> news) {
+        this.news = news;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull  ViewGroup parent, int viewType) {
+        View view=LayoutInflater.from(context).inflate(R.layout.new_item,parent,false);
+        return new ViewHolder(view);
+//        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull  NewsAdapter.ViewHolder holder, int position) {
+
+        holder.txtTitle.setText(news.get(position).getTitle());
+        holder.txtContext.setText(news.get(position).getDescription());
+        holder.txtDate.setText(news.get(position).getDate());
+        holder.parent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: 7/15/2021 navigete to web activity
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        try{
+            return news.size();
+        }catch (Exception e){
+            e.printStackTrace();
+            return 0;
+        }
+
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        private TextView txtTitle,txtContext,txtDate;
+        private CardView parent;
+        public ViewHolder( View itemView) {
+            super(itemView);
+            txtContext=itemView.findViewById(R.id.txtContent);
+            txtTitle=itemView.findViewById(R.id.txtTitle);
+            txtDate=itemView.findViewById(R.id.txtDate);
+            parent=itemView.findViewById(R.id.parent);
+        }
+    }
+}
